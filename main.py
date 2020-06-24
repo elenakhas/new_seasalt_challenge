5edbffe4df6e42abf6fd71b7151d4ec163cc3ff3
elenakhas <yelena.khas@gmail.com>
2020-06-24 00:02:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ from torchvision import datasets, transforms
 
 
 class Net(nn.Module):
-    '''The definition of the network'''
+    '''The class describing the network'''
 
     def __init__(self):
-        '''Initialize the network'''
+        '''Initialize the network with two convolution and two linear layers'''
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 20, 5, 1)
         self.conv2 = nn.Conv2d(20, 50, 5, 1)
@@ -21,7 +21,7 @@ class Net(nn.Module):
         self.fc2 = nn.Linear(500, 10)
 
     def forward(self, x):
-        '''Define the forward layer'''
+        '''Define the forward pass'''
         x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, 2, 2)
         x = F.relu(self.conv2(x))
